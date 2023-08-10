ebed8d92f3ecb2c1b7ff79f1db73c1a6a0c630e3
Jagane Sundar <jagane@infinstor.com>
2023-08-10 15:26:16 -0700

    fix comments, add Dockerfile for ggml model

--- llama2-7b-chat-ggml/log.py
+++ llama2-7b-chat-ggml/log.py
@@ -16,12 +16,7 @@ con = {
     ],
 }
 
-# messages must be of the following format
-# system|user|function: [name]: content
-# Examples:
-# system: : Answer in a consistent style.
-# user: Joe.User: Teach me about patience
-#input_schema = Schema([ColSpec(DataType.string, None, False)])
+# role: system, user or assistant
 input_schema = Schema([ColSpec(DataType.string, "role", False), ColSpec(DataType.string, "message", False)])
 
 # Return messages are of the following format
@@ -37,3 +32,4 @@ print(sign)
 with mlflow.start_run():
     print(f"Using model data {sys.argv[1]}")
     mlflow.pyfunc.log_model("model", loader_module='customloader.loader', data_path=sys.argv[1], code_path=['customloader'], conda_env=con, signature=sign)
+    mlflow.log_artifact('Dockerfile', artifact_path='model')
